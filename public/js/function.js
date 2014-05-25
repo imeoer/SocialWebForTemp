2f2f43167be99238983fb6749acbe443d6c7455d
imeoer <imeoer@gmail.com>
2014-05-26 02:13:45 +0800

    fix login logic

--- public/js/function.js
+++ public/js/function.js
@@ -1,17 +1,33 @@
-$(document).ready(function(){
-
-	// login
-
-
-	$('#register').on('click', function() {
-
-		$.post('register', {
-			user_name: $('#register_email').val(),
-			pass_word: $('#register_password').val()
-		}, function(res) {
-			console.log(res);
-			
-		});
-
+$(function() {
+	
+	// 绑定登录单击事件
+	$('.submit_l').on('click', function() {
+		var userName = $('#username').val();
+		var passWord = $('#password').val();
+		// 登录请求
+		$.post('/login', {
+			user_name: userName,
+			pass_word: passWord
+		}, function(result) {
+			if (result.success) {
+				window.location = '/';
+			} else {
+				alert(result.data);
+			}
+		}, 'JSON');
 	});
+
+	// 绑定注册单击事件
+	$('.submit_r').on('click', function() {
+		var userName = $('#reg_username').val();
+		var passWord = $('#reg_password').val();
+		// 注册请求
+		$.post('/register', {
+			user_name: userName,
+			pass_word: passWord
+		}, function(result) {
+			alert(result.data);
+		}, 'JSON');
+	});
+
 });
